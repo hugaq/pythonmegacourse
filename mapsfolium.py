d5978333ef041e17d353cfff8b47898c7eb1085e
hugaq <spicedham@posteo.org>
2019-02-02 18:21:09 +0100

    volcanoes

--- mapsfolium.py
+++ mapsfolium.py
@@ -1,5 +1,11 @@
 import folium
+import pandas
 
 
-map = foilum.Map(location=[80, -100])
+volcanoes = pandas.read_csv('Volcanoes.txt', sep=',')
+volcanoes = volcanoes.astype({'LAT':'object', 'LON':'object'})
+volcanoes['MAP'] = volcanoes['LAT'] + ', ' + volcanoes['LON']
+print(volcanoes.dtypes)
+map = folium.Map(tiles='Mapbox Bright')
+
 map.save('Map1.html')
