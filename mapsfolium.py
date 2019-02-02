0c38e0a050ff161a83adab6033d0b8394e357ca9
hugaq <spicedham@posteo.org>
2019-02-02 18:32:58 +0100

    doesn't work

--- mapsfolium.py
+++ mapsfolium.py
@@ -3,9 +3,9 @@ import pandas
 
 
 volcanoes = pandas.read_csv('Volcanoes.txt', sep=',')
-volcanoes = volcanoes.astype({'LAT':'object', 'LON':'object'})
+volcanoes = volcanoes.astype({'LAT':'str', 'LON':'str'})
 volcanoes['MAP'] = volcanoes['LAT'] + ', ' + volcanoes['LON']
-print(volcanoes.dtypes)
+print(volcanoes)
 map = folium.Map(tiles='Mapbox Bright')
-
+volcanoes['MAP'].apply(map.add_child(folium.Marker))
 map.save('Map1.html')
