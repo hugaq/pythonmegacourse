e7909911a7282fb35ca7a61abb25535714c44d5c
hugaq <spicedham@posteo.org>
2019-02-02 18:39:12 +0100

    hm

--- mapsfolium.py
+++ mapsfolium.py
@@ -7,5 +7,8 @@ volcanoes = volcanoes.astype({'LAT':'str', 'LON':'str'})
 volcanoes['MAP'] = volcanoes['LAT'] + ', ' + volcanoes['LON']
 print(volcanoes)
 map = folium.Map(tiles='Mapbox Bright')
-volcanoes['MAP'].apply(map.add_child(folium.Marker))
+def func(location):
+    inp = location.split(',')
+    map.add_child(folium.Marker(location=[int(inp[0]), int(inp[1])]))
+volcanoes['MAP'].apply(func)
 map.save('Map1.html')
