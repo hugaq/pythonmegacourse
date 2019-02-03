838295555cd69edfd40eb73bd133a919e5ae7cc2
hugaq <spicedham@posteo.org>
2019-02-03 19:30:05 +0100

    small changes

--- numpy image read.py
+++ numpy image read.py
@@ -1,8 +1,7 @@
 import cv2
 
 image = cv2.imread('smallgray.png', 0)
+print(image)
 image[0,0] = 255
 print(image)
 cv2.imwrite('newsmallgrey.png', image)
-for i in range(image.shape):
-    print(i)
--- pandas_usecases.py
+++ pandas_usecases.py
@@ -3,5 +3,6 @@ import json
 
 
 df = pd.read_json('supermarkets.json')
-df.loc[6] = ['asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd']
+df.loc[df.shape[0]] = ['asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd']
+df.loc[df.shape[0]] = ['asd', 'asd', 'asd', 'asd', 'asd', 'asd', 'asd']
 print(df)
