77c147c2cc1311172dd6d211702322d20c729941
hugaq <spicedham@posteo.org>
2019-02-09 17:03:51 +0100

    websiteblocker init

--- websiteblocker.py
+++ websiteblocker.py
@@ -1,6 +1,13 @@
 import time
+from time import gmtime, strftime
 
 
 hosts_path = '/etc/hosts'
 redirect_address = '127.0.0.1'
 websites_blocked = ['heise.de', 'zeit.de']
+
+
+
+
+print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+print(strftime("%M", gmtime()))
