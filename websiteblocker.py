ab6def309576a94ba8f0d3af965ebbad9a4591d9
Johannes Schukat <j.schukat@posteo.net>
2019-02-10 22:49:58 +0100

    all(list(map

--- websiteblocker.py
+++ websiteblocker.py
@@ -9,5 +9,19 @@ websites_blocked = ['heise.de', 'zeit.de']
 
 
 
-print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
-print(strftime("%M", gmtime()))
+#print(strftime("%Y-%m-%d %H:%M:%S", gmtime()))
+#print(strftime("%M", gmtime()))
+while True:
+    if strftime("%M", gmtime()) < '59':
+        with open('hosts', 'r+') as hosts:
+            data = hosts.read()
+            if all(list(map(lambda x: True if x in data else False, websites_blocked))):
+                print('all in')
+            else:
+                print('write it in')
+                for i in websites_blocked:
+                    hosts.write(i)
+        #print('Work Work Work')
+    else:
+        print('Happy Hour')
+    time.sleep(5)
