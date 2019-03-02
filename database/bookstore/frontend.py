80ad301948b2c3ea906230aff137a6ad785ccac2
hugaq <19303816+hugaq@users.noreply.github.com>
2019-03-02 09:33:53 +0100

    bookstore

---
+++ database/bookstore/frontend.py
@@ -0,0 +1,12 @@
+"""
+A program that stores book information:
+Title, Author, Year, ISBN
+
+User can:
+    View all records (Scrollbar)
+    Search an entry
+    Add an entry
+    Update an entry
+    Delete an entry
+    Close the application
+"""
--- database/postgresql.py
+++ database/postgresql.py
@@ -1,24 +1,25 @@
-import sqlite3
+import psycopg2
 
 
 def create_table():
-    with sqlite3.connect('lite.db') as conn:
+    with psycopg2.connect("dbname='dbname' user='user' password='password' host='host' port='5432'") as conn:
         cur = conn.cursor()
         cur.execute('CREATE TABLE IF NOT EXISTS STORE (ITEM TEXT, QUANTITY INTEGER, PRICE REAL)')
         conn.commit()
 
 def insert(item, quantity, price):
-    with sqlite3.connect('lite.db') as conn:
+    with psycopg2.connect('lite.db') as conn:
         cur = conn.cursor()
         cur.execute('INSERT INTO STORE VALUES (?, ?, ?)', (item, quantity, price))
         conn.commit()
 
 def view():
-    with sqlite3.connect('lite.db') as conn:
+    with psycopg2.connect('lite.db') as conn:
         cur = conn.cursor()
         cur.execute('SELECT * FROM STORE')
         return cur.fetchall()
 
-
+"""
 insert('Coffee Cup', 5, 22.2)
 print(view())
+"""
