a0d644452205ef34042a571048163940be544545
hugaq <19303816+hugaq@users.noreply.github.com>
2019-03-03 03:15:45 +0100

    book store done

--- bookstore/backend.py
+++ bookstore/backend.py
@@ -1,1 +1,49 @@
 import sqlite3
+
+
+def connect(db):
+    with sqlite3.connect(db) as conn:
+        cur = conn.cursor()
+        return {'connection':conn, 'cursor':cur}
+
+class database():
+    def __init__(self, db='books.db'):
+        c = connect(db)
+        self.connection = c['connection']
+        self.cursor = c['cursor']
+        self.cursor.execute("""CREATE TABLE IF NOT EXISTS BOOK (ID INTEGER PRIMARY KEY NOT NULL, TITLE TEXT, AUTHOR TEXT, YEAR INTEGER, ISBN INTEGER)""")
+        self.connection.commit()
+
+    def insert(self, title, author, year, isbn):
+        self.cursor.execute("INSERT INTO BOOK VALUES(NULL, ?, ?, ?, ?)",
+                            (title, author, year, isbn))
+        self.connection.commit()
+
+    def view(self):
+        self.cursor.execute("SELECT * FROM BOOK")
+        return self.cursor.fetchall()
+
+    def search(self, title=None, author=None, year=None, isbn=None, id=None):
+        self.cursor.execute("SELECT * FROM BOOK WHERE TITLE=? OR AUTHOR=? OR YEAR=? OR ISBN=? OR ID=?",
+                            (title, author, year, isbn, id))
+        return self.cursor.fetchall()
+
+    def delete(self, id):
+        self.cursor.execute("DELETE FROM BOOK WHERE ID=?", (id,))
+        self.connection.commit()
+
+    def update(self, id, title=None, author=None, year=None, isbn=None):
+        status_quo = self.search(id=id)[0]
+        if not title:
+            title = status_quo[1]
+        if not author:
+            author = status_quo[2]
+        if not year:
+            year = status_quo[3]
+        if not isbn:
+            isbn = status_quo[4]
+        self.cursor.execute("UPDATE BOOK SET TITLE=?, AUTHOR=?, YEAR=?, ISBN=? WHERE ID=?",
+                            (title, author, year, isbn, id))
+        self.connection.commit()
+
+db = database()
--- bookstore/frontend.py
+++ bookstore/frontend.py
@@ -13,7 +13,7 @@ User can:
 
 # Use Tkinter for python 2, tkinter for python 3
 import tkinter as tk
-import backend
+from backend import *
 
 
 class Lists(tk.Listbox):
@@ -28,10 +28,22 @@ class Lists(tk.Listbox):
             else:
                 pass
         tk.Listbox.__init__(self, master=parent, **list_properties)
+        self.bind('<<ListboxSelect>>', self.get_selected_row)
         self.grid(row=row, column=column, **grid_properties)
+
+    def view_command(self, lst):
+        self.delete(0, tk.END)
+        for row in lst:
+            self.insert(tk.END, row)
 
     def config(self, **kwargs):
         self.configure(**kwargs)
+
+    def get_selected_row(self, event):
+        global tpl
+        index = self.curselection()[0]
+        tpl = str(self.get(index)[0])
+        print(tpl)
 
 class Labels(tk.Label):
     def __init__(self, parent, row, column, Text):
@@ -43,6 +55,9 @@ class Entries(tk.Entry):
         self.text_variable = tk.StringVar()
         tk.Entry.__init__(self, master=parent, textvariable=self.text_variable)
         self.grid(row=row, column=column)
+
+    def out(self):
+        return self.text_variable.get()
 
 class Scrollbars(tk.Scrollbar):
     def __init__(self, parent, row, column, **kwargs):
@@ -57,9 +72,10 @@ class Buttons(tk.Button):
         tk.Button.__init__(self, master=parent, text=text, width=width, command=command)
         self.grid(row=row, column=column)
 
+
 class MainApplication():
     def __init__(self, parent, *args, **kwargs):
-        self.parent = parent
+        #self.parent = parent
         Labels(parent=parent, row=0, column=0, Text='Title')
         e1 = Entries(parent=parent, row=0, column=1)
         Labels(parent=parent, row=0, column=2, Text='Author')
@@ -76,16 +92,38 @@ class MainApplication():
         l1.config(yscrollcommand=sb1.set)
         sb1.config(command=l1.yview)
 
-        b1 = Buttons(parent=parent, row=2, column=3, text='View all', width=12)
-        b2 = Buttons(parent=parent, row=3, column=3, text='Search entry', width=12)
-        b3 = Buttons(parent=parent, row=4, column=3, text='Add entry', width=12)
-        b4 = Buttons(parent=parent, row=5, column=3, text='Update', width=12)
-        b5 = Buttons(parent=parent, row=6, column=3, text='Delete', width=12)
-        b6 = Buttons(parent=parent, row=7, column=3, text='Close', width=12)
-        #<create the rest of your GUI here>
+
+
+        b1 = Buttons(parent=parent, row=2, column=3, text='View all', width=12, command=lambda: l1.view_command(db.view()))
+        b2 = Buttons(parent=parent, row=3, column=3, text='Search entry', width=12, command=lambda: self.search_command(l1, e1, e2, e3, e4))
+        b3 = Buttons(parent=parent, row=4, column=3, text='Add entry', width=12, command=lambda: self.insert_command(l1, e1, e2, e3, e4))
+        b4 = Buttons(parent=parent, row=5, column=3, text='Update', width=12, command=lambda: self.update_command(l1, e1, e2, e3, e4))
+        b5 = Buttons(parent=parent, row=6, column=3, text='Delete', width=12, command=lambda: self.delete_command(l1))
+        b6 = Buttons(parent=parent, row=7, column=3, text='Close', width=12, command=parent.destroy)
+
+    def search_command(self, l1, e1, e2, e3, e4):
+        result = db.search(title=e1.out(), author=e2.out(), year=e3.out(), isbn=e4.out())
+        l1.view_command(result)
+
+    def insert_command(self, l1, e1, e2, e3, e4):
+        db.insert(title=e1.out(), author=e2.out(), year=e3.out(), isbn=e4.out())
+        l1.view_command(['insert successful'])
+
+    def delete_command(self, l1):
+        db.delete(tpl)
+        l1.view_command(['entry successfully deleted'])
+
+    def update_command(self, l1, e1, e2, e3, e4):
+        db.update(id=tpl, title=e1.out(), author=e2.out(), year=e3.out(), isbn=e4.out())
+        l1.view_command(['entry updated'])
+
+
+
+
 
 if __name__ == "__main__":
     root = tk.Tk()
+    root.wm_title("Book Store")
     MainApplication(root)#.pack(side="top", fill="both", expand=True)
 
     root.mainloop()
