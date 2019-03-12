d80de34921b27470ce1202dab5bf6a0718b42bef
Johannes Schukat <j.schukat@posteo.net>
2019-03-12 22:49:02 +0100

    bank

--- bankaccount/account.py
+++ bankaccount/account.py
@@ -1,12 +1,21 @@
 class account():
-    def __init__(self, balance, accountnumber):
-        self.balance = balance
-        self.accountnumber = accountnumber
+    def __init__(self, filepath):
+        self.filepath = filepath
+        with open(filepath, 'r') as file:
+            data = file.read().splitlines()
+            try:
+                self.balance = int(data[-1])
+            except:
+                self.balance = 0
 
     def withdrawl(self, withdrawl):
         self.balance -= withdrawl
 
     def deposit(self, deposit):
         self.balance += deposit
+
+    def commit(self):
+        with open(self.filepath, 'a') as file:
+            file.write(str(self.balance))
 
 def main():
