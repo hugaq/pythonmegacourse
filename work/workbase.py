23c3a6fbb0a550325c3bc70d90630b6b0768b9e5
hugaq <19303816+hugaq@users.noreply.github.com>
2019-03-26 20:23:49 +0100

    workworkworkworkwork

--- work/workbase.py
+++ work/workbase.py
@@ -1,9 +1,47 @@
+"""
+Purpose: record working hours in a simple and easy manner and eventually depict
+         them visually.
+
+Input: Either via commandline or from within the program. In both cases without
+       GUI. Date can be given at the beginning, if this is not done, current date
+       and time will be inserted.
+"""
 import sqlite3
 import datetime
 import sys
 
 
-def 
+def time_padding(timepiece):
+    timepieces = timepiece.split(':')
+    for i in range(2):
+        if len(timepieces[i]) == 1:
+            timepieces[i] = '0'+timepieces[i]
+    return ':'.join(timepieces)
+
+def date_padding(datepiece):
+    datepieces = datepiece.split('.')
+    for i in range(len(datepieces)):
+        if len(datepieces[i]) == 1:
+            datepieces[i] = '0'+datepieces[i]
+    if len(datepieces) == 1:
+        datepieces.append(datetime.datetime.now().strftime('%m.%y'))
+    elif len(datepieces) == 2:
+        datepieces.append(datetime.datetime.now().strftime('%y'))
+    return '.'.join(datepieces)
+
+def determine_datetime(timestamp):
+    pit = datetime.datetime.now().strftime('%d.%m.%y %H:%M').split(' ')
+    return_dict = {'time':pit[1], 'date':pit[0]}
+    if timestamp:
+        parts = timestamp.split(' ')
+        for part in parts:
+            if ':' in part:
+                return_dict['time'] = time_padding(part)
+            else:
+                return_dict['date'] = date_padding(part)
+    return f"{return_dict['date']} {return_dict['time']}"
+
+
 class database():
     def __init__(self, db='work.db'):
         with sqlite3.connect(db) as conn:
@@ -47,3 +85,4 @@ class database():
 
 #db = database()
 arg_input = sys.argv
+print(determine_datetime(arg_input[1]))
